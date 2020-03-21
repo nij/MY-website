e5be68ce153e875c0d60f0c261dd0d5a4bcb7664
nij <yann.raude@gmail.com>
2020-03-21 19:06:55 +0100

    fix intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -13,19 +13,19 @@ const IntroPage = () => (
         <p class="adentum">Mise à jour COVID 19</p>
         <h3>Chers vous,</h3>
         <p>
-          Vous l’aurez surement compris au regard de la situation actuelle, mais il est impossible pour nous de maintenir notre mariage ce 11 avril… <br>
-Voilà quelques jours que cette évidence s’est imposée à nous, mais il nous fallait le temps de gérer quelques urgences avant de revenir vers vous.<br>
+          Vous l’aurez surement compris au regard de la situation actuelle, mais il est impossible pour nous de maintenir notre mariage ce 11 avril… <br/>
+Voilà quelques jours que cette évidence s’est imposée à nous, mais il nous fallait le temps de gérer quelques urgences avant de revenir vers vous.<br/>
 On aimerait pouvoir vous donner une autre date, mais pour l’instant - et pour citer un grand philosophe - “on y voit pas plus loin qu’à l’aveuglette”.. donc difficile de vous en dire plus.
         </p>
         <p>
-          On est forcément (très) déçu, un peu (beaucoup) triste, désolé aussi d’annuler alors que vous aviez pris du temps pour venir fêter ça avec nous jusque sur une petite île bretonne.<br>
+          On est forcément (très) déçu, un peu (beaucoup) triste, désolé aussi d’annuler alors que vous aviez pris du temps pour venir fêter ça avec nous jusque sur une petite île bretonne.<br/>
 On espère aussi que cette annulation ne sera pas trop compliquée à gérer pour chacun, vu l’organisation et les moyens engagés.
         </p>
  <p>
   Mais le plus important est encore que vous passiez cette drôle de période en bonne santé, vous et vos proches. 
   </p>
         <p>
-          Et soyez assurés que l’on reviendra vers vous quand nous aurons trouvé ou et quand célébrer notre mariage, toujours à venir et toujours avec vous.<br>
+          Et soyez assurés que l’on reviendra vers vous quand nous aurons trouvé ou et quand célébrer notre mariage, toujours à venir et toujours avec vous.<br/>
 C’était important pour nous de le vivre avec les gens qui comptent, ça le sera d’autant plus quand tout ça sera terminé.
         </p>
 <p>On vous embrasse fort<br>Marina et Yann</p>
